11ac07135bab065c6061b41d79556278aafa7866
BasemSabry <basemsabry000@gmail.com>
2021-03-31 16:54:08 +0200

    ValidationErrors

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
@@ -8,16 +9,17 @@ import { MessagesComponent } from './messages/messages.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 const routes: Routes = [
+  {path:"",component:HomeComponent},
   {path:"",runGuardsAndResolvers:"always",canActivate:[AuthGuard],
   children:[
-    {path:"",component:HomeComponent},
     {path:"members",component:MemberListComponent,canActivate:[AuthGuard]},
     {path:"members/:id",component:MemberDetailComponent},
     {path:"lists",component:ListsComponent},
     {path:"messages",component:MessagesComponent},
-    {path:"**",component:MemberListComponent},
   ]
 },
+{path:"errors",component:TestErrorsComponent},
+{path:"**",component:MemberListComponent},
 
 
 ];
