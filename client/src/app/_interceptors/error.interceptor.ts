662b037ec6b55f614dc46446d1d220748294ae30
BasemSabry <basemsabry000@gmail.com>
2021-04-01 11:17:11 +0200

    end of section 7

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -39,7 +39,8 @@ export class ErrorInterceptor implements HttpInterceptor {
               break;
               case 404: this.router.navigateByUrl('/not-found')
               break;
-              case 500:const navigationExtras : NavigationExtras= {state:{error:error.error}}
+              case 500:
+              const navigationExtras : NavigationExtras= {state:{error:error.error}}
               this.router.navigateByUrl('/server-error',navigationExtras)
               break;
             default:
